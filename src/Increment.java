49fedb3cbe7f5aea8e1d0ef02ebbb15dc6857d1e
Dr. Kat <katychuang@users.noreply.github.com>
2022-06-07 12:35:34 -0400

    Increment

--- src/Increment.java
+++ src/Increment.java
@@ -1,3 +1,29 @@
 // Starting with integer i with value 0, increment until it is 10.
 
+public class Increment {
+  public static void main(String[] a){
+    int i = 0;
+    i = i + 1;
+    System.out.println(i); 
+    i = i + 1;
+    System.out.println(i); 
+    i = i + 1;
+    System.out.println(i); 
+    i = i + 1;
+    System.out.println(i); 
+    i = i + 1;
+    System.out.println(i); 
+    i = i + 1;
+    System.out.println(i); 
+    i = i + 1;
+    System.out.println(i); 
+    i = i + 1;
+    System.out.println(i); 
+    i = i + 1;
+    System.out.println(i); 
+    i = i + 1;
+    System.out.println(i); 
+  }
+}
+
 
