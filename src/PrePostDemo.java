218263b9f33868cea5461767bd101bedb6cf4881
Dr. Kat <katychuang@users.noreply.github.com>
2022-06-08 11:48:10 -0400

    Examples shown in class

---
+++ src/PrePostDemo.java
@@ -0,0 +1,20 @@
+// Example shows prefix and postfix.
+// from https://thinkjava.org/operators
+
+class PrePostDemo {
+    public static void main(String[] args){
+        int i = 3;
+        i++;
+        // prints 4
+        System.out.println(i);
+        ++i;			   
+        // prints 5
+        System.out.println(i);
+        // prints 6
+        System.out.println(++i);
+        // prints 6
+        System.out.println(i++);
+        // prints 7
+        System.out.println(i);
+    }
+}
